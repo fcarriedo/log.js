fba6de05e90bf4828d796774057c0709b1b261c6
Francisco Carriedo <fcarriedo@gmail.com>
2013-02-20 11:49:32 -0800

    Updated swith statment to object hash

--- log.js
+++ log.js
@@ -1,25 +1,10 @@
+// The log mechanism
 var log = (function(levl, useAlert) {
-  var level = 2; // Defaults to 'info'
-  switch(levl) {
-    case 'error':
-      level = 0;
-      break;
-    case 'warn':
-      level = 1;
-      break;
-    case 'info':
-      level = 2;
-      break;
-    case 'debug':
-      level = 3;
-      break;
-    case 'mute':
-      level = -1;
-      break;
-  }
+  var options = {error: 0, warn: 1, info: 2, debug:4, mute: -1};
+  var level = options[levl] || 2; // Defaults to info
 
-  var useAlert = !!useAlert;
-  var console = window.console || {log: function(msg) {if(useAlert) alert(msg)}};
+  // Use console if existent, otherwise replace it with alert if useAlert = true
+  var console = window.console || {log: function(msg) {if(!!useAlert) alert(msg)}};
 
   return {
     error: function(msg) {
@@ -35,4 +20,4 @@ var log = (function(levl, useAlert) {
       if(level >= 3) console.log('debug: ' + msg);
     }
   }
-})('debug');
+})();
